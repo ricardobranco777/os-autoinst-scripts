ricardobranco777/os-autoinst-scripts

commit a3b288b6202c85516f31846ccb492c72a3f694f0

fix(rpm): exclude not anymore supported Python 3.6 testing on Leap 15.6
This fixes a problem trying to execute tests on Leap 15.6 based on the
default Python 3.6 version as tests are not compatible with this old
Python version anymore.

dist/rpm/os-autoinst-scripts-deps.spec
@@ -15,7 +15,11 @@
 # Please submit bugfixes or comments via http://bugs.opensuse.org/
 #
 
-
+%if 0%{?suse_version} <= 1560
+%bcond_without tests
+%else
+%bcond_with tests
+%endif
 %define         base_name os-autoinst-scripts
 Name:           %{base_name}-deps
 Version:        1
@@ -59,8 +63,10 @@ in %{base_name}-deps
 %install
 
 %check
+%if %{with tests}
 # Can't run bash tests because it needs to clone bpan via git
 make test-python
+%endif
 
 %files
 

commit 759471fa767558963fd1fb28406830606b223e26

fix(rpm): fix wrongly turned around exclusion of tests
This fixes a regression in a3b288b6.

dist/rpm/os-autoinst-scripts-deps.spec
@@ -16,9 +16,9 @@
 #
 
 %if 0%{?suse_version} <= 1560
-%bcond_without tests
-%else
 %bcond_with tests
+%else
+%bcond_without tests
 %endif
 %define         base_name os-autoinst-scripts
 Name:           %{base_name}-deps